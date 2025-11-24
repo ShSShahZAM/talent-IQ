ca65971c5a7b21ddbdffaffcae9f146d2aad9bfb
ShahZam <theshahzam@gmail.com>
2025-11-24 12:12:54 +0900

    initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,33 @@
 //const express = require('express');
 import express from 'express';
-import dotenv from 'dotenv';
+import path from 'path';
+
 import { ENV } from './lib/env.js';
 
 dotenv.config();
 
 const app = express();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+const __dirname = path.resolve();
 
 app.get('/health', (req, res)=> {
     res.status(200).send('api is up and running');
 })
+
+app.get('/books', (req, res)=> {
+    res.status(200).send('this is books endpoint');
+})
+
+
+
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+    
 app.listen(ENV.PORT, ()=> console.log('Server is running on port:', ENV.PORT));
